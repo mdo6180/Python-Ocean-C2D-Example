00c4d3c4658cf67c0a862730771128765435a552
Ubuntu <ubuntu@ip-172-31-54-222.ec2.internal>
2022-06-09 23:13:54 +0000

    example.py outputs result in json file

--- example.py
+++ example.py
@@ -6,11 +6,18 @@ import sys
 def line_counter(path):
     with open(path, "r") as file:
         data = json.load(file)
-        print(len(data))
+        line_count = len(data)
+        return line_count
 
 
 if __name__ == "__main__":
     print(f"counting up number of rows in {sys.argv[1]}")
     path = sys.argv[1]
-    line_counter(path)
 
+    result = line_counter(path)
+    output = {"line_counter_result":result}
+
+    # Serializing json 
+    json_object = json.dumps(output, indent = 4)
+    with open("output.json", "w") as outfile:
+        outfile.write(json_object)
